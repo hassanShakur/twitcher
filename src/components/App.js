c7c067cafd2539097b4c488eeca80fe159902f35
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-29 22:12:34 +0300

    Boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
-import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' component={StreamCreate} />
+          <Route path='/streams/edit' component={StreamEdit} />
+          <Route path='/streams/show' component={StreamShow} />
+          <Route path='/streams/delete' component={StreamDelete} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
